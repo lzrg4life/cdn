55e2bc9f3243861ffb5f6733bb55a1c50d357077
Willis Stearns <52726855+lzrg4life@users.noreply.github.com>
2021-08-22 14:08:49 -0400

    Add more jsdoc comments

--- dist/js/helpers.js
+++ dist/js/helpers.js
@@ -1,7 +1,16 @@
+/**
+ * Shortcut for document.createElement.
+ */
 export const ce = document.createElement;
+
+/**
+ * Shortcut for document.getElementById.
+ */
 export const gebi = document.getElementById;
 
 /**
+ * Shortcut for creating an element with some common attributes.
+ * 
  * tagName: The html tag name to be passed to document.createElement
  * className: (optional) The value for the html class attribute
  * innerText: (optional) The value to set the element's innerText to
